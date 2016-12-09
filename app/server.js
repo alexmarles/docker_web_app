35d63d0115bfd8d6146b2cb183c10c7fb5fb059a
Àlex Marlés <alex@alexmarles.com>
2016-12-09 17:05:36 +0100

    Testing product API endpoints

--- app/server.js
+++ app/server.js
@@ -8,25 +8,33 @@ const mongoose   = require('mongoose');
 mongoose.connect('mongodb://mongo:27017');
 
 // App
-const app = express();
-const port = process.env.PORT || 3000;
+const app      = express();
+const port     = process.env.PORT || 3000;
+const STATUS   = {
+  OK: 200,
+  NOT_FOUND: 404
+}
+
 app.set('port', port);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/api/product', (req, res) => {
+app.get('/api/products', (req, res) => {
+  res.status(STATUS.OK).send({ products: [] });
 });
 
-app.get('/api/product/:product_id', (req, res) => {
+app.get('/api/product/:productId', (req, res) => {
 });
 
 app.post('/api/product', (req, res) => {
+  console.log(req.body);
+  res.status(STATUS.OK).send({ message: 'Product received' });
 });
 
-app.put('/api/product/:product_id', (req, res) => {
+app.put('/api/product/:productId', (req, res) => {
 });
 
-app.delete('/api/product/:product_id', (req, res) => {
+app.delete('/api/product/:productId', (req, res) => {
 });
 
 app.get('/', (req, res) => {
