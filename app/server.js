f2fa79cbe090254743413b53e687eb7d899f7af4
Àlex Marlés <alex@alexmarles.com>
2016-12-09 15:58:24 +0100

    Added API endpoints for products

--- app/server.js
+++ app/server.js
@@ -10,14 +10,18 @@ app.set('port', port);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/api/product', (req, res) => {
+});
+
+app.get('/api/product/:product_id', (req, res) => {
+});
+
+app.post('/api/product', (req, res) => {
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!\n')
 });
-
-app.get('/:nombre', (req, res) => {
-  res.send(`Hola ${req.params.nombre}!`);
-});
-
 
 app.listen(app.get('port'), (err) => {
   console.log(`Server running on http://localhost:${app.get('port')}`);
