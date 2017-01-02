a1075caba65fa45eb445235a4339f151a4de855a
Àlex Marlés <alex@alexmarles.com>
2017-01-02 14:17:47 +0100

    Added keys file for private data

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
+// Keys Import
+import keys from './keys';
+
 // App Config Variables
 export default {
   port: process.env.PORT || 3000,
   db: process.env.MONGODB || 'mongodb://mongo:27017',
-  SECRET_TOKEN: 'thisismytoken'
+  SECRET_TOKEN: keys.login.secret
 };
