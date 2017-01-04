a2d32d7ba3b8329847b70a624b72bd62be5cbe96
Àlex Marlés <alex@alexmarles.com>
2017-01-04 20:06:23 +0100

    Login working with password checking

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import mongoose     from 'mongoose';
+import bcrypt       from 'bcrypt-nodejs';
 import User         from '../models/user';
 import authService  from '../services/auth';
 
@@ -9,6 +10,12 @@ function signUp (req, res) {
     email: req.body.email,
     displayName: req.body.displayName,
     password: req.body.password
+  });
+
+  User.findOne({ email: user.email }, (err, userFound) => {
+    if (err) return res.status(500).send({ message: `Error while checking for duplicates in database: ${err}` });
+
+    if (userFound) return res.status(409).send({ message: `User with this email is already in database` });
   });
 
   user.save((err, userStored) => {
@@ -21,12 +28,19 @@ function signUp (req, res) {
 function signIn (req, res) {
   User.findOne({
     email: req.body.email
-  }, (err, user) => {
+  }).select('password').exec((err, user) => {
     if (err) return res.status(500).send({ message: `Error while signing user: ${err}` });
     if (!user) return res.status(404).send({ message: 'Authentication failed. User not found.' });
-    if (user.password != req.body.password) return res.status(401).send({ message: 'Authentication failed. Wrong password.' });
 
-    res.status(200).send({ token: authService.createToken(user) });
+    let match = bcrypt.compareSync(req.body.password, user.password);
+    if (!match) return res.status(401).send({ message: 'Authentication failed. Wrong password.' });
+
+    user.lastLoginAt = Date.now();
+    user.save((err, userStored) => {
+      if (err) return res.status(500).send({ message: `Error while saving user in database: ${err}` });
+
+      res.status(200).send({ token: authService.createToken(userStored) });
+    });
   });
 }
 
